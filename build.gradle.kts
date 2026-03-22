6468da40bd05cc95f74101b5d34019b2834609f4
locnguyenx <loc.nguyenxuan@gmail.com>
2026-03-22 23:17:43 +0700

    build: add Spring Security, JWT, and Kafka dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,8 +20,14 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springframework.boot:spring-boot-starter-security")
 
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.5.0")
+    implementation("io.jsonwebtoken:jjwt-api:0.12.5")
+    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.5")
+    runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.5")
+
+    implementation("org.springframework.kafka:spring-kafka")
 
     implementation("org.flywaydb:flyway-core")
     // runtimeOnly("org.postgresql:postgresql")  # Commented out for H2 development mode
