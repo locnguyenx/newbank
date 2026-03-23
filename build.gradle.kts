da2a4a7ec1178766b313494f4a23f321a6cadc05
unknown <LocNX6@BANK-LocNX6.fsoft.fpt.vn>
2026-03-23 12:04:55 +0700

    fix(build): add application plugin and mainClass for bootRun

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     java
     id("org.springframework.boot") version "3.2.5"
     id("io.spring.dependency-management") version "1.1.4"
+    id("application")
 }
 
 group = "com.banking"
@@ -10,6 +11,10 @@ version = "0.0.1-SNAPSHOT"
 java {
     sourceCompatibility = JavaVersion.VERSION_17
     targetCompatibility = JavaVersion.VERSION_17
+}
+
+application {
+    mainClass.set("com.banking.BankingApplication")
 }
 
 repositories {
